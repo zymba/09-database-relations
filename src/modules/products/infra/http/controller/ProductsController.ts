84279c11f7e83f8ade3d5e4c99fd655428999b97
Bueno <zymbabueno@gmail.com>
2020-06-18 19:44:25 -0300

    Create products

--- src/modules/products/infra/http/controller/ProductsController.ts
+++ src/modules/products/infra/http/controller/ProductsController.ts
@@ -4,7 +4,17 @@ import { container } from 'tsyringe';
 import CreateProductService from '@modules/products/services/CreateProductService';
 
 export default class ProductsController {
-  public async create(request: Request, response: Response): Promise<void> {
-    // TODO
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name, price, quantity } = request.body;
+
+    const createProduct = container.resolve(CreateProductService);
+
+    const product = await createProduct.execute({
+      name,
+      price,
+      quantity,
+    });
+
+    return response.json(product);
   }
 }
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,7 +21,15 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
